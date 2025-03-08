feffa74a3bd1c822c645de0f69ade200882c80c5
Eduardo Henrique Fritsche <edu.fritsche@gmail.com>
2025-03-08 15:26:58 -0300

    Reorganiza os loops na clase Main

--- src/main/java/gestao/eventos/Main.java
+++ src/main/java/gestao/eventos/Main.java
@@ -2,12 +2,7 @@ package gestao.eventos;
 
 import gestao.eventos.config.BancoDeDados;
 import gestao.eventos.config.PreenchimentoBanco;
-import gestao.eventos.entidades.eventos.*;
-import gestao.eventos.entidades.*;
 import gestao.eventos.opcoes.ControleOpcoes;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -18,57 +13,84 @@ public class Main {
         BancoDeDados banco = BancoDeDados.getInstancia();
         PreenchimentoBanco.preenche(banco); // preenche as classes para exemplificar o sistema
         ControleOpcoes controleOpcoes = ControleOpcoes.getInstancia(); // gerencia as opcoes do sistema
-        int opção;
+        int opcao;
 
+        // do while inicial
         do {
-            //Adicionar o resto dos Cases quando terminarmos o resto
-            System.out.println("Qual a operação desejada");
-            System.out.println("1. Cadastrar Participante");
-            System.out.println("2. Cadastrar Palestrante");
-            System.out.println("3. Cadastrar Musico");
-            System.out.println("4. Cadastrar Local");
-            System.out.println("5. Cadastrar Palestra");
-            System.out.println("6. Cadastrar Show Musical");
-            System.out.println("7. Exibir Participantes");
-            System.out.println("8. Exibir Palestrantes");
-            System.out.println("9. Exibir Musicos");
-            System.out.println("10. Exibir Locais");
-            System.out.println("11. Exibir Palestras");
-            System.out.println("12. Exibir Show Musicais");
-            System.out.println("13. Deletar Participante");
-            System.out.println("14. Deletar Palestra");
-            System.out.println("15. Alterar Participante");
-            System.out.println("16. Alterar Palestra");
-            System.out.println("0. Encerrar o Programa");
+            System.out.println("Digite o número da operação desejada: ");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Exibir");
+            System.out.println("3 - Remover");
+            System.out.println("4 - Alterar");
+
 
             System.out.println("Opção: ");
-            opção = scanner.nextInt();
+            opcao = scanner.nextInt();
             scanner.nextLine(); // limpa o buffer do scanner para nao pular linha
 
-            switch (opção){
+            // switch inicial
+            switch (opcao){
 
-                case 1:{
+                case 1:{// Opcao de Cadastros
+                    int opcaoCadastro = 0;
+
+                    do {
+                        System.out.println("Digite a opção para CADASTRAR: ");
+                        System.out.println("1 - Participante");
+                        System.out.println("2 - Palestrante");
+                        System.out.println("3 - Musico");
+                        System.out.println("4 - Local");
+                        System.out.println("5 - Palestra");
+                        System.out.println("6 - Show Musical");
+                        System.out.println("0 - Sair do cadastro");
+
+                        System.out.println("Opção: ");
+                        opcaoCadastro = scanner.nextInt();
+                        scanner.nextLine(); // limpa o buffer do scanner para nao pular linha
+
+                        switch (opcaoCadastro) {
+                            case 1: // Cadastra Participante
+                                System.out.println("cadastra participante");
+                                break;
+                            case 2:// Cadastra Palestrante
+                                controleOpcoes.getOpcao("cadastra palestrante").executaOpcao();
+                                break;
+                            case 3:// Cadastra Músico
+                                System.out.println("cadastra musico");
+                                break;
+                            case 4:// Cadastra Local
+                                controleOpcoes.getOpcao("cadastra local").executaOpcao();
+                                break;
+                            case 5:// Cadastra Palestra
+                                controleOpcoes.getOpcao("cadastra palestra").executaOpcao();
+                                break;
+                            case 6:// Cadastra Show Musical
+                                controleOpcoes.getOpcao("cadastra show musical").executaOpcao();
+                                break;
+                            case 0:// Sair da Opção de Cadastro
+                                System.out.println("Saindo do cadastro");
+                                break;
+                            default:// Opção Inválida
+                                System.out.println("Opção inválida");
+                                break;
+                        }
+                    } while(opcaoCadastro != 0);
 
                     break;
                 }
-                case 2:
-                    controleOpcoes.getOpcao("cadastra palestrante").executaOpcao();
+                case 2:// Opção de Exibir
+                    System.out.println("exibir");
                     break;
-                case 3:
+                case 3:// Opção de Remover
+                    System.out.println("remover");
                     break;
-                case 4: // CADASTRA LOCAL
-                    controleOpcoes.getOpcao("cadastra local").executaOpcao();
-                    break;
-                case 5: //CADASTRA PALESTRA
-                    controleOpcoes.getOpcao("cadastra palestra").executaOpcao();
-                    break;
-                case 7: //CADASTRA SHOW MUSICAL
-                    controleOpcoes.getOpcao("cadastra show musical").executaOpcao();
+                case 4: // Opção de Alterar
+                    System.out.println("alterar");
                     break;
                 case 0:
                     System.out.println("Encerrando o programa");
                     break;
             }
-        } while(opção != 0);
+        } while(opcao != 0);
     }
 }
